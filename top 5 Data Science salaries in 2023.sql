4fb8f4c9e03a18180adb0743dfdd921f1e7ae5ba
OlhaMatvieieva <olha.matveeva96@gmail.com>
2024-06-05 14:17:22 +0200

    Update top 5 Data Science salaries in 2023.sql

--- top 5 Data Science salaries in 2023.sql
+++ top 5 Data Science salaries in 2023.sql
@@ -11,4 +11,12 @@ WHERE
 ORDER BY salary_in_usd DESC
 LIMIT 5;
 
--- 
+-- Average, min, max salary in 2023
+
+SELECT
+	AVG(salary_in_usd) AS salary_avg
+	, MIN(salary_in_usd) AS salary_min
+	, MAX (salary_in_usd) AS salary_max
+FROM salaries
+WHERE year = '2023'
+LIMIT 10;
