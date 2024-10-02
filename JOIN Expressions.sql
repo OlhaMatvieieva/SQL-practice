396ce96a4bd7dac5f04981fcd4ccae1b6f0c7979
OlhaMatvieieva <olha.matveeva96@gmail.com>
2024-10-02 23:09:04 +0200

    create new file

---
+++ JOIN Expressions.sql
@@ -0,0 +1,22 @@
+SELECT 
+	t.TrackId 
+	, t.Name 
+	, a.Title 
+	, a.ArtistId 
+	, art.Name 
+FROM Track t 
+JOIN Album a ON t.AlbumId = a.AlbumId 
+JOIN Artist art ON a.ArtistId = art.ArtistId
+WHERE art.Name LIKE "A%"
+LIMIT 100;
+
+SELECT 
+	art.Name 
+	, COUNT(t.TrackId) 
+FROM Track t 
+JOIN Album a ON t.AlbumId = a.AlbumId 
+JOIN Artist art ON a.ArtistId = art.ArtistId
+WHERE art.Name LIKE "A%"
+GROUP BY 1
+ORDER BY 2 DESC 
+LIMIT 100;
